cc84ce308d772deec6b5df2536830390afd61431
alfredo-rivara <matias.rivara.t@gmail.com>
2024-10-01 22:21:41 -0300

    Agregamos la vista tweets/busqueda al routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "tweets/busqueda", to: "tweets#busqueda", as: :busqueda_tweets
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "tweets#index"
 end
